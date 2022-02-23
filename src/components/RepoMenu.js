ee2f4bad08a93c8342b3551f10546fd8f98e8dd3
Tampoka <goal.thanks@gmail.com>
2022-02-23 15:03:37 +0900

    Add MUI.

--- src/components/RepoMenu.js
+++ src/components/RepoMenu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {RepoInfo} from './RepoInfo';
 
-const RepoMenu = ({repositories,onSelect=f=>f}) => {
+const RepoMenu = ({repositories,selected,onSelect=f=>f}) => {
     if(!repositories.length) return <p>No repositories found...</p>
 
     return <RepoInfo  repositories={repositories}
